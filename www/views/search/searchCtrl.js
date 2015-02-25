7243949779d4a47820fbb5b9bab6c7feb29d486d
Gaurav Dhamija <gaurav.dhamija@software-innovation.com>
2015-02-25 14:13:31 -0500

    Add settings and search view

---
+++ www/views/search/searchCtrl.js
@@ -0,0 +1,11 @@
+angular.module('weatherApp')
+	   .controller('SearchCtrl', function ($scope, $http) {
+	   		$scope.model = {term: ''};
+
+	   		$scope.search = function() {
+	   			$http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: {address:$scope.model.term}})
+	   				 .success(function(response){
+	   				 	$scope.results = response.results;
+	   				 });
+	   		}	
+	   });
